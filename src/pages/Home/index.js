c4842c88f81dc2121694085c0b574d11731f6621
audisiofilho <obuzua46@gmail.com>
2022-05-20 11:07:20 -0300

    Iniciando projeto

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>pagina home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
