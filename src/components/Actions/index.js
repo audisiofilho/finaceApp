e84d7f399fc2a435e1c8546feff7ddf409bfd71b
audisiofilho <obuzua46@gmail.com>
2022-05-20 14:16:19 -0300

    Finalizando ActionTab

---
+++ src/components/Actions/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Actions() {
+  return (
+    <ScrollView
+      style={styles.container}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+    >
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="addfolder" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Entradas</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="tagso" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Compras</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="creditcard" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Carteira</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="barcode" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Boleto</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="codepen" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Pix</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="setting" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Conta</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    maxHeight: 84,
+    marginBottom: 14,
+    marginTop: 18,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  actionButton: {
+    alignItems: "center",
+    marginRight: 32,
+  },
+  areaButton: {
+    backgroundColor: "#ecf0f1",
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  labelButton: {
+    marginTop: 4,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, FlatList } from "react-native";
+import Actions from "../../components/Actions";
 import Balance from "../../components/Balance";
 import Header from "../../components/Header";
 import Moviments from "../../components/Moviments";
@@ -6,41 +7,42 @@ import Moviments from "../../components/Moviments";
 const list = [
   {
     id: 1,
-    label:'Boleto Conta de luz',
-    value:'299,90',
-    date: '20/05/2022',
-    type: 0
+    label: "Boleto Conta de luz",
+    value: "299,90",
+    date: "20/05/2022",
+    type: 0,
   },
   {
     id: 2,
-    label:'Pix leonardo',
-    value:'150,00',
-    date: '20/05/2022',
-    type: 1
+    label: "Pix leonardo",
+    value: "150,00",
+    date: "20/05/2022",
+    type: 1,
   },
   {
     id: 3,
-    label:'Salário',
-    value:'4.550,66',
-    date: '15/05/2022',
-    type: 1
-  }
-]
+    label: "Salário",
+    value: "4.550,66",
+    date: "15/05/2022",
+    type: 1,
+  },
+];
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header userName={"Nataniele Lima"} />
       <Balance saldo={"4.560,65"} gastos={"673,00"} />
+      <Actions />
 
       <Text style={styles.title}>Últimas movimentações</Text>
 
       <FlatList
         style={styles.list}
         data={list}
-        keyExtractor={(item)=> String(item.id)}
+        keyExtractor={(item) => String(item.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={({item})=> <Moviments data={item}/>}
+        renderItem={({ item }) => <Moviments data={item} />}
       />
     </View>
   );
@@ -51,15 +53,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fafafa",
   },
-  title:{
-    fontSize:18,
-    fontWeight:'bold',
-    marginLeft: 14,
-    marginRight: 14,
-    marginTop: 14,
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 14,
   },
-  list:{
+  list: {
     marginStart: 14,
     marginEnd: 14,
-  }
+  },
 });
